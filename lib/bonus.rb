88cfb922c26beace47343f857a6d98aed905db51
FutureScript <fernandobaxter1@gmail.com>
2018-11-20 20:44:37 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -26,8 +26,7 @@ def bonus
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
-  [:montague][:hero][:status]=> "dead"
-  [:capulet][:hero][:status]=> "dead"
+  [:montague][:hero][:status]= "dead"
+  [:capulet][:hero][:status]= "dead"
   epic_tragedy
 end
-
